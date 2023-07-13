b23302443fa07fa64685208cd834e52f21012bc1
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-07-13 09:17:25 -0400

    Complete Home Page Dom Javascript

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,40 @@
 const BASE_URL = "https://www.gamerpower.com/api/giveaways"
+const main = document.querySelector("#game-giveaway-list")
+
+window.addEventListener("load", getFreeGames() => {
+	event.preventDefault()
+	fetch(BASE_URL)
+	.then(response => console.log(response))
+	.then(response => gameList(response))
+	.catch(error => displayError(error))
+}
+
+
+const gameList = (response) => {
+	main.innerHTML = `<article>
+	<img src="${json.image}">
+	<br>
+	<h3>${json.title}</h3>
+	<br>
+	<span class="description">${json.description}</span>
+	<br>
+	<span class="type">${json.type}</span>
+	<br>
+	<span class="platforms">${json.platforms}</span>
+	<br>
+	<span class="open-giveaway-url">${json.open_giveaway_url}</span>
+	 </article>`
+}
+
+const displayError = (error) => {
+	main.innerHTML = <div class="error"> 
+	<p>Our Apologies. There seem to be an error</p>
+	<p class="error-message">${error}</p>
+	</div>
+}
+
+
+
+
+
+
