0a4f45fbab95b148f76590fbdcff3372e95b3e74
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-07-16 10:58:48 -0400

    Update Project

--- scripts/main.js
+++ scripts/main.js
@@ -2,43 +2,34 @@ const api =
   "https://newsapi.org/v2/everything?q=gaming&apiKey=8f1e6a243d7b47da88ee3c9589d37437";
 
 const main = document.querySelector(".main");
+const section = document.querySelector("#cards");
 
 window.addEventListener("load", (event) => {
   fetch(api)
     .then((data) => data.json())
-    .then((json) => gamingNews(json));
+    .then((json) => gamingNews(json))
+    .catch((error) => displayError(error))
 });
 
 const gamingNews = (json) => {
   const articles = json.articles;
-  console.log(articles);
- for (const article of articles) {
+ articles.forEach(article => {
   console.log(article)
-    main.innerHTML = `<div class="wrapper">
-    <div class="card">
-      <div class="card-banner">
-        <p class="articles-source">${article.source.name}</p>
-        <img class="banner-img" src=${article.urlToImage} alt=''>
-      </div>
-      <div class="card-body">
-        <h2 class="articles-title">${article.title}</h2>
-        <p class="articles-description">${article.description}</p>
-
-        <div class="card-author">
-          <div class="card-author-info">
-            <h3 class="author-name">${article.author}</h3>
-            <p class="articles-url">${article.url}</p>
-          </div>
-        </div>
-      </div>
-    </div>`;
- }
+  section.innerHTML = `<div class="card">
+  <img src=${article.urlToImage}  style="width:100%">
+  <h1>${article.title}</h1>
+  <p class="author">${article.author}</p>
+  <p>${article.description}</p>
+  <p><button onclick=${article.url}>${article.source.name}</button></p>
+</div>`
+main.append(section)
+ })
 }
 
 
-// const displayError = (error) => {
-// 	main.innerHTML = `<article id="error">
-// 	<span>We're Sorry. There seem to be an error</span>
-// 	<span class="error-message>${error}</span>
-// 	</article>`
-// }
+const displayError = (error) => {
+	section.innerHTML = `<article id="error">
+	<span>We're Sorry. There seem to be an error</span>
+	<span class="error-message>${error}</span>
+	</article>`
+}
